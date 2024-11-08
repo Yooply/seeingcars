f98a446b7239b84856e28f01eae2b06faf2771ee
John Phillips <johnphillips@sandiego.edu>
2024-11-08 11:11:59 -0800

    [wip]threaded processing

--- facetester.py
+++ facetester.py
@@ -1,19 +1,35 @@
 import cv2
 import sys
+import threading
 from deepface import DeepFace
 
 cascPath = sys.argv[1]
 verifyPath = sys.argv[2]
-faceCascade = cv2.CascadeClassifier(cascPath)
+# faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
 counter = 0
+
+spacer = 30
+
+def identify(image):
+    obj = DeepFace.verify(verifyPath, image, model_name='ArcFace', detector_backend="fastmtcnn")
+    if obj["verified"] == True:
+        print(obj)
+        sys.stdout.write("Match\n")
+    else:
+        sys.stdout.write("False\n")
+    sys.stdout.flush()
+
+threadHolder: list[threading.Thread] = []
+
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    
+    '''
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
@@ -24,14 +40,26 @@ while True:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    '''
 
     # Display the resulting frame
 
     cv2.imshow('Video', frame)
-    if len(faces) > 0:
-        cv2.imwrite(f"livestream_frame.jpg", gray)
-        obj = DeepFace.verify(verifyPath, "livestream_frame.jpg", model_name='ArcFace', detector_backend="retinaface")
-        print(obj["verified"])
+    if spacer == 120:
+        spacer = 0
+        img = frame.copy(); 
+        t = threading.Thread(target=identify, kwargs={"image":img})
+        t.start()
+        threadHolder.append(t)
+    else:
+        spacer += 1
+        delIdx = []
+        for i, t in enumerate(threadHolder):
+            t.join(timeout=0.001)
+            if not t.is_alive():
+                delIdx.append(i)
+        for i in delIdx:
+            threadHolder.pop(i)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
